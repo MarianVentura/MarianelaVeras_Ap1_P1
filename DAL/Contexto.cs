a8ef3892de89c23af808d067ba2cfe5e14eaff65
MarianVentura <marianv1119@gmail.com>
2024-09-23 21:40:10 -0400

    Cree el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using MarianelaVeras_Ap1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarianelaVeras_Ap1_P1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Registro> Registro { get; set; }
+}
