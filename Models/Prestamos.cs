9be134afbbd4c8dc0e36350ab887952658cd8040
MarianVentura <marianv1119@gmail.com>
2024-09-30 18:33:02 -0400

    Cree el Modelo Prestamos

---
+++ Models/Prestamos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarianelaVeras_Ap1_P1.Models;
+
+public class Prestamos
+{
+    [Key]
+    public int PrestamoId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string? Deudor {  get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string? Concepto { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public decimal Monto { get; set; }
+
+
+}
