4a1f5491fef7bd73a911d70563d053740aa6d056
MarianVentura <marianv1119@gmail.com>
2024-10-07 19:43:14 -0400

    Cree el modelo Deudor

---
+++ Models/Deudor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarianelaVeras_Ap1_P1.Models
+{
+    public class Deudor
+    {
+        [Key]
+        public int DeudorId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Nombres {  get; set; }
+    }
+}
