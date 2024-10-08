b34d8798f2a7d038854ba9476c7f20ab70e4e781
MarianVentura <marianv1119@gmail.com>
2024-10-08 11:03:21 -0400

    Modifique el modelo de Prestamos para agregar al Deudor en este.

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MarianelaVeras_Ap1_P1.Models;
 
@@ -6,15 +7,20 @@ public class Prestamos
 {
     [Key]
     public int PrestamoId { get; set; }
-
-    [Required(ErrorMessage = "Campo Obligatorio")]
-    public string? Deudor {  get; set; }
-
+    
     [Required(ErrorMessage = "Campo Obligatorio")]
     public string? Concepto { get; set; }
 
     [Required(ErrorMessage = "Campo Obligatorio")]
     public decimal Monto { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public decimal Balance { get; set; }
+    public int DeudorId { get; set; }
+
+    [ForeignKey("DeudorId")]
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public Deudores? Deudor { get; set; }
 
 
 }
