746190699f28f0fd3a24e4c9ebc857e73e625801
MarianVentura <marianv1119@gmail.com>
2024-10-10 22:37:13 -0400

    Agregue Cobros y CobroDetalles al contexto

---
+++ Models/CobroDetalles.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarianelaVeras_Ap1_P1.Models;
+
+public class CobroDetalles
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [ForeignKey("Cobros")]
+    public int CobroId { get; set; }
+
+    [ForeignKey("Prestamos")]
+    public int PrestamoId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public decimal ValorCobrado { get; set; }
+
+    public Cobros? Cobro { get; set; }
+    public Prestamos? Prestamo { get; set; }
+}
